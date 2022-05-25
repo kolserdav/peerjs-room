d6de53efddf29fd6e6f07e877916e37e344d8572
kolserdav <serega12101983@gmail.com>
2022-05-25 17:59:18 +0700

    Set up playwright

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,47 @@
 import cluster from 'cluster';
-import path  from "path";
-import fs from "fs";
-import { PeerServer } from './peerjs-server/index'
+import path from 'path';
+import fs from 'fs';
+import { chromium } from 'playwright';
+import { PeerServer } from './peerjs-server/index';
+
+const VIEWPORT = { width: 1633, height: 768 };
+// eslint-disable-next-line prefer-const
+let recordVideo: any = { dir: 'videos/', size: VIEWPORT };
+// TODO
+// recordVideo = undefined;
 
 if (cluster.isPrimary) {
   const worker = cluster.fork();
   worker.on('message', (m) => {
-    console.log(m)
+    console.log(m);
   });
-
   worker.on('disconnect', () => {
-    console.error('Disconnect')
+    console.error('Disconnect');
   });
+  (async () => {
+    const browser = await chromium.launch({
+      headless: true,
+      devtools: true,
+      args: [
+        '--allow-file-access-from-files',
+        '--disable-gesture-requirement-for-media-playback',
+        '--use-fake-ui-for-media-stream',
+        '--use-fake-device-for-media-stream',
+      ],
+    });
+    const context = await browser.newContext({ recordVideo: recordVideo || undefined });
+    const page = await context.newPage();
+    await page.setViewportSize(VIEWPORT);
+    await page.goto('http://localhost:3000');
+    await page.waitForTimeout(5000);
+    await page.close();
+  })();
 } else if (process.send) {
-  
   const peer = PeerServer({
     path: '/',
     port: 9000,
-    host: ''
+    host: '',
   });
-
 } else {
   console.warn('Unhandled case');
 }
